71275f5b53a542e0e310b1fe619dc79e3417b7eb
Harel <harel@Harels-MacBook-Pro.local>
2025-10-30 12:47:17 +0200

    My 4th HW Attempt

--- NumWords.java
+++ NumWords.java
@@ -1,10 +1,10 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
-		int number = Integer.parseInt(args[0]); //0<=number<=999
-		int hundreds = (number % 1000) / 100;
-		//int hundreds = number / 100; //Iproved it
+		// Replace this comment with your code
+		int number = Integer.parseInt(args[0]); // 0<=number<=999
+		// int hundreds = (number % 1000) / 100;
+		int hundreds = number / 100; // Improved it
 		int tens = (number % 100) / 10;
 		int units = number % 10;
 		int[] numWordArray = new int[3];
@@ -12,7 +12,7 @@ public class NumWords {
 		numWordArray[1] = tens;
 		numWordArray[2] = units;
 		System.out.print(numWordArray[0] + " hundreds, " +
-		 					numWordArray[1] + " tens, and " +
-							numWordArray[2] + " ones.");
+				numWordArray[1] + " tens, and " +
+				numWordArray[2] + " ones.");
 	}
 }
